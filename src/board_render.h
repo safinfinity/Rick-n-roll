caaa1d99c48668ebb16c661e0d6d636e0ebf419c
Tasmiah Tanha <tasmiahtanhaa@gmail.com>
2026-09-13 19:34:55 +0600

    comments added

--- src/board_render.h
+++ src/board_render.h
@@ -1,9 +1,9 @@
-#ifndef BOARD_RENDER_H
+#ifndef BOARD_RENDER_H  //the beginning of a header guard, which prevents the same header from being included multiple times
 #define BOARD_RENDER_H
 
 #include "game.h"
 
-void board_draw(Game *g);
-void board_draw_hud(Game *g);
+void board_draw(Game *g);    
+void board_draw_hud(Game *g);//HUD means Heads-Up Display,the HUD is the information displayed around the game screen
 
 #endif
